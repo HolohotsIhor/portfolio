9c1cac6b9e534baae7513904feed7f392b668c9e
airSlateHolohots <ihor.holohots@airslate.com>
2025-05-12 10:33:51 +0300

    Create clean project

---
+++ src/components/SectionSubtitle/SectionSubtitle.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './SectionSubtitle.module.scss';
+
+type SectionSubtitleProps = {
+    text: string;
+};
+
+export const SectionSubtitle: React.FC<SectionSubtitleProps> = ({ text }) => {
+    return (
+        <p className={styles.sectionSubtitle}>
+            { text }
+        </p>
+    );
+}
